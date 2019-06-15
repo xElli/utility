f940cf8423de71b56275486c35cb75b94c451d7a
rebecca <rebecca.marzahn@outlook.com>
2019-06-15 19:32:27 +0200

    Utility, second commit

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    thumb = models.ImageField(default='defaul.png', blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
